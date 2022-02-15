fb52b42b27d20444f55306d4fdf4b0f084134a49
Inny Choi <innychoi93@gmail.com>
2022-02-15 16:53:08 -0600

    completed rating star

---
+++ client/src/component/ratings/Ratings.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+import { Container, Radio, Rating } from './Ratings.styles';
+
+function StarRatings({ currentRate, isRating }) {
+  const [rate, setRate] = useState(0);
+  return (
+    <Container>
+      {[...Array(5)].map((item, index) => {
+        const givenRating = index + 1;
+        return (
+          <>
+            {isRating
+            && (
+            <label>
+              <Radio
+                type="radio"
+                value={givenRating}
+                onClick={() => {
+                  setRate(givenRating);
+                }}
+              />
+              <Rating>
+                <FaStar
+                  color={
+                givenRating < rate || givenRating === rate
+                  ? '000'
+                  : 'rgb(192,192,192)'
+              }
+                />
+              </Rating>
+            </label>
+            )}
+            {!isRating
+            && (
+            <label>
+              <Radio
+                type="radio"
+                value={givenRating}
+              />
+              <Rating>
+                <FaStar
+                  color={
+                givenRating < currentRate || givenRating === currentRate
+                  ? '000'
+                  : 'rgb(192,192,192)'
+              }
+                />
+              </Rating>
+            </label>
+            )}
+          </>
+        );
+      })}
+    </Container>
+  );
+}
+
+// StarRatings.propTypes = {
+//   currentRate: PropTypes.number,
+// };
+
+export default StarRatings;
