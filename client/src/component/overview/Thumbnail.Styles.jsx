522207ca7ece5ed61aa89e64e04c81b69c101312
Grant Sanchez <granchez77@gmail.com>
2022-02-18 20:30:42 -0800

    more rebase fun got styles dynamically rendering via grid

--- client/src/component/overview/Thumbnail.Styles.jsx
+++ client/src/component/overview/Thumbnail.Styles.jsx
@@ -1,27 +1,23 @@
 import styled from 'styled-components';
 import { Container } from '../../GlobalStyle';
 
+export const Picture = styled(Container)`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 0.25rem;
+  grid-auto-rows: 150px;
+`;
 // export const Picture = styled(Container)`
-//   display: inline;
+//   display: inline-flex;
 //   text-align: justify;
 //   justify-content: space-around;
 //   align-items: center;
 //   background-color: white;
 //   height: 100%;
-//   width: 50%;
+//   width: 100%;
+
 //   ${Container}
 // `;
-export const Picture = styled(Container)`
-  display: inline-flex;
-  text-align: justify;
-  justify-content: space-around;
-  align-items: center;
-  background-color: white;
-  height: 100%;
-  width: 100%;
-
-  ${Container}
-`;
 
 export const ThumbnailImage = styled.div`
   background-image: ${({ src }) => `url(${src})`};
@@ -30,8 +26,11 @@ export const ThumbnailImage = styled.div`
   border-radius: 50%;
   width: 100px;
   height: 100px;
+  display: inline-block;
+  position: relative;
+  left: 13px;
 `;
 
 export const Text = styled.div`
-  display: inline
+  display: inline-block;
 `;
